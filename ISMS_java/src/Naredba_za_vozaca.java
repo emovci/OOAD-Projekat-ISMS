5c450e07e39b173295e497fdd7c5bd6a63365f1e
broj6 <ehasanovic1@etf.unsa.ba>
2015-05-03 15:45:56 +0200

    Update

---
+++ ISMS_java/src/Naredba_za_vozaca.java
@@ -0,0 +1,69 @@
+
+public class Naredba_za_vozaca {
+	private Ruta[] rute;
+	private Datum vrijeme_polaska;
+	private Datum vrijeme_dolaska;
+	private boolean naredba_prihvacena;
+	private boolean naredba_izvrsena;
+	
+	
+	public void set_naredba_prihvaæena(boolean prihvacenost) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean get_naredba_prihvacena() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Ruta[] get_rute() {
+		throw new UnsupportedOperationException();
+	}
+
+
+	public void set_rute(Ruta[] rute) {
+		throw new UnsupportedOperationException();
+	}
+
+	
+	public void dodaj_rutu(Ruta ruta) {
+		throw new UnsupportedOperationException();
+	}
+
+	
+	public void ukloni_rutu(int red_broj_rute) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean provjeri_optimalnost() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void razlozi_neoptimalnosti() {
+		throw new UnsupportedOperationException();
+	}
+
+
+	public void set_neke_rute_zavrsene(int[] broj_ruta) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void set_naredba_zavrsena(boolean završenost) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean get_naredba_zavrsena() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Datum get_vrijeme_polaska() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Datum get_vrijeme_dolaska() {
+		throw new UnsupportedOperationException();
+	}
+
+	public double cijena_naredbe() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ ISMS_java/src/Vozac.java
@@ -0,0 +1,23 @@
+
+public class Vozac extends Generalisani_radnik {
+
+	private Naredba_za_vozaca[] trenutna_naredba;
+	private Kamion kamion;
+
+	public Naredba_za_vozaca[] get_trenutna_naredba() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void set_trenutna_naredba(Naredba_za_vozaca[] naredba) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Kamion get_kamion() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void set_kamion(Kamion kamion) {
+		throw new UnsupportedOperationException();
+	}
+
+}
