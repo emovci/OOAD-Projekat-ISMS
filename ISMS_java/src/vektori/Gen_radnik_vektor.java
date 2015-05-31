d923ecdc33a6164dc54b78949055d9a25f9605d5
Kurburis <skrilasevi1@etf.unsa.ba>
2015-05-31 20:42:42 +0200

    Patch 7.5.5

--- ISMS_java/src/vektori/Gen_radnik_vektor.java
+++ ISMS_java/src/vektori/Gen_radnik_vektor.java
@@ -5,12 +5,15 @@ import klase.Datum;
 import klase.Generalisani_radnik;
 import klase.Kamion;
 import klase.Naredba_za_vozaca;
+import klase.Naredba_za_radnika;
 import klase.Vozac;
-
+import klase.Radnik;
 public class Gen_radnik_vektor 
 {
+
 		private Vector <Generalisani_radnik> radnici=new Vector <Generalisani_radnik> ();
-		private int zadnji_id=0;
+		private int zadnji_id;
+
 		
 		public Vector <Generalisani_radnik> get_radnike ()
 		{
@@ -29,6 +32,11 @@ public class Gen_radnik_vektor
 				zadnji_id++;
 		}
 		
+		public void dodaj_radnika (String ime, String prezime, Datum datum_rodjenja, boolean status, double plata_dnevnica, Vector <Naredba_za_radnika> trenutna_naredba)
+		{
+				radnici.addElement(new Radnik(ime, prezime, datum_rodjenja, status, zadnji_id, plata_dnevnica, trenutna_naredba));
+				zadnji_id++;
+		}
 		
 		public void ukloni_radnika (int redni_broj)
 		{
